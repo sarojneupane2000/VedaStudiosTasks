11710ef368900d902676ba176961ce6e277d1b81
sarojneupane2000 <neupanesaroj198@gmail.com>
2024-07-26 12:14:41 +0545

    completed the project with responsive design also

--- components/About.tsx
+++ components/About.tsx
@@ -12,9 +12,9 @@ const About = () => (
             We advocate for vulnerable animals, providing safety while promoting responsible pet ownership. We uplift both animals and communities.
           </p>
         </div>
-        <div className="mt-10 lg:flex lg:items-center lg:space-x-8">
+        <div className="mt-20 lg:flex lg:items-start lg:space-x-8">
           <div className="lg:w-1/2 lg:pr-8 text-center lg:text-left">
-            <p className="text-lg leading-6 text-gray-500">
+            <p className="text-3xl leading-9 font-extrabold text-gray-900">
               Your donations help us run important projects
             </p>
             <p className="mt-4 text-lg leading-6 text-gray-500">
@@ -27,7 +27,7 @@ const About = () => (
           <div className="mt-10 lg:mt-0 lg:w-1/2">
             <div className="relative">
               <img
-                className="mx-auto rounded-lg shadow-lg w-full sm:w-3/4 lg:w-full"
+                className="mx-auto rounded-lg shadow-lg w-full sm:w-2/3 lg:w-3/4"
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.d9JZlAUcUGmGy9tZKLvpvAHaHa%26pid%3DApi&f=1&ipt=386590b15232b996a37f57e0645b7e0764deab301d19ebf34aee4f35d4965d94&ipo=images"
                 alt="Animals"
               />
